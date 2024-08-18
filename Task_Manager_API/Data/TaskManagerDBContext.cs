81df26a4d5a0cf8129836c21f79377840609786a
Revanth P <revanthhatvern@outlook.com>
2024-08-18 14:55:12 +0530

    Add project files.

---
+++ Task_Manager_API/Data/TaskManagerDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task_Manager_API.Models.Domain;
+
+namespace Task_Manager_API.Data
+{
+    public class TaskManagerDBContext : DbContext
+    {
+        public TaskManagerDBContext(DbContextOptions<TaskManagerDBContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    
+            public DbSet<TaskManager> Tasks { get; set; }
+    }
+}
---
+++ Task_Manager_API/Models/Domain/TaskManager.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_Manager_API.Models.Domain
+{
+    public class TaskManager
+    {
+        public int Id { get; set; }
+
+        public string TaskName { get; set; }
+
+        public string? TaskDescription { get; set; }
+
+        public int TargetTime {  get; set; }
+
+    }
+}
